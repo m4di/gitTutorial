30ccdd3cd583871b47744550cdfd5340ae1b3c9b
m4di <mglew@uwo.ca>
2017-02-15 14:31:20 -0500

    Added Course class and made changes to Student and Instructor

---
+++ src/Course.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Madi on 2017-02-15.
+ */
+public class Course {
+    String cname;
+    String description;
+    Course favCourse;
+
+    public Course(String cname, String description, Course favCourse){
+        this.cname = cname;
+        this.description = description;
+        this.favCourse = favCourse;
+    }
+}
